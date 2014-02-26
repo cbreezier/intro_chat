d31db391c02b89d42e5909b51a3ce1b74532e9ce
Davy Mao <e_equals_mass_speed_light_squared@hotmail.com>
2014-02-27 01:22:37 +1100

    changing message object

--- receiver.py
+++ receiver.py
@@ -8,17 +8,11 @@ import sqlite3
 import pickle
 import datetime
 
-class Message(object):
-    def __init__(self, user, message, time):
-        self.user = user
-        self.message = message
-        self.time = time
-
 postvars = cgi.FieldStorage()
 connection = sqlite3.connect("messages.db")
 cursor = connection.cursor()
 
-newMessage = Message(postvars["user"], postvars["message"], datetime.datetime.now())
+newMessage = {"user": postvars["user"], "message": postvars["message"], "timestamp": datetime.datetime.now()}
 
 cursor.execute("INSERT INTO messages '" + (pickle.dumps(newMessage)) + "';")
 connection.commit()
