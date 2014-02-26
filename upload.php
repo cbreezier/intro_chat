00ddd55a0711b8ecd215baf6486c39b360f32df8
Leo Huang <crazysheep2302@gmail.com>
2014-02-26 22:20:42 +1100

    More meaningful error messages on upload and return button

--- upload.php
+++ upload.php
@@ -1,17 +1,16 @@
 <?php
   if ($_FILES["file"]["error"] > 0) {
-    echo "<pre>Error: " . $_FILES["file"]["error"] . "</pre>";
+    $message = "<b>Error: ".$_FILES["file"]["error"]."</b> Please select a file to upload.";
   } else {
     // echo "Upload: " . $_FILES["file"]["name"] . "<br>";
     // echo "Type: " . $_FILES["file"]["type"] . "<br>";
     // echo "Size: " . ($_FILES["file"]["size"] / 1024) . " kB<br>";
     // echo "Stored in: " . $_FILES["file"]["tmp_name"];
-    $message = '';
     $extension = end(explode(".", $_FILES["file"]["name"]));
     if ($_FILES["file"]["type"] !== 'text/plain' || $extension !== 'py') {
-      $message = "You must upload a valid .py file, not a ".$_FILES["file"]["type"]." .".$extension." file. Please try again.";
+      $message = "You must upload a valid .py file, not a ".$_FILES["file"]["type"]." .".$extension." file. Please try again.<br>";
     } else {
-      $message .= "Your file is OK and has been uploaded!<br>";
+      $message = "Your file is OK and has been uploaded!<br>";
       move_uploaded_file($_FILES["file"]["tmp_name"], "chatbots/" . $_FILES["file"]["name"]);
     }
   }
@@ -31,6 +30,8 @@
     <div class="container">
       <h1>Upload File Page</h1>
       <p class="lead"><?=$message?></p>
+      <br>
+      <a href="index.php">Return to chat</a>
     </div>
   </body>
 </html>
