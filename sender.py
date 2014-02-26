b087cec1430478aa7eeeafb1defad4fd5aa8eebf
Davy <e_equals_mass_speed_light_squared@hotmail.com>
2014-02-27 03:29:26 +1100

    fixing time

--- sender.py
+++ sender.py
@@ -8,6 +8,7 @@ import sqlite3
 import datetime
 import pickle
 import json
+import time
 
 postvars = cgi.FieldStorage()
 # postvars = {}
@@ -15,14 +16,20 @@ postvars = cgi.FieldStorage()
 connection = sqlite3.connect("messages.db")
 cursor = connection.cursor()
 
+slept = 0
+
 returnedMessages = []
-dateTimeFormat = "%I:%M:%S %d/%m/%y"
-for object in cursor.execute("SELECT object FROM messages"):
-    message = pickle.loads(str(object[0]))
-    if not "last_message" in postvars or message["timestamp"] > datetime.datetime.strptime(postvars["last_message"].value, dateTimeFormat):
-        message["message_time"] = message["timestamp"].strftime(dateTimeFormat)
-        message.pop("timestamp")
-        returnedMessages.append(message)
+
+while not returnedMessages and slept < 50:
+    dateTimeFormat = "%I:%M:%S.%f %d/%m/%y"
+    for object in cursor.execute("SELECT object FROM messages"):
+        message = pickle.loads(str(object[0]))
+        if not "last_message" in postvars or message["timestamp"] > datetime.datetime.strptime(postvars["last_message"].value, dateTimeFormat):
+            message["message_time"] = message["timestamp"].strftime(dateTimeFormat)
+            message.pop("timestamp")
+            returnedMessages.append(message)
+    time.sleep(0.1)
+    slept += 1
 
 connection.close()
 
