68668996b18297ee89e3ca92d9165f864b589c7c
Davy Mao <e_equals_mass_speed_light_squared@hotmail.com>
2014-02-27 01:46:21 +1100

    no more debug variables

--- receiver.py
+++ receiver.py
@@ -9,12 +9,12 @@ import pickle
 import datetime
 
 postvars = cgi.FieldStorage()
-postvars = {}
+# postvars = {}
 connection = sqlite3.connect("messages.db")
 cursor = connection.cursor()
 
-postvars["user"] = "test"
-postvars["message"] = "test"
+# postvars["user"] = "test"
+# postvars["message"] = "test"
 newMessage = {"user": postvars["user"], "message": postvars["message"], "timestamp": datetime.datetime.now()}
 
 cursor.execute("INSERT INTO messages VALUES(?);", (pickle.dumps(newMessage),))
