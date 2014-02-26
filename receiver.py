162ce21a2735e161bb4ad6422368e7f9cfdf9fa6
Davy Mao <e_equals_mass_speed_light_squared@hotmail.com>
2014-02-27 02:34:53 +1100

    fixed sender, changed db

--- receiver.py
+++ receiver.py
@@ -7,6 +7,7 @@ import cgi
 import sqlite3
 import pickle
 import datetime
+import time
 
 postvars = cgi.FieldStorage()
 # postvars = {}
@@ -16,8 +17,12 @@ cursor = connection.cursor()
 # postvars["user"] = "test"
 # postvars["message"] = "test"
 newMessage = {"user": postvars["user"], "message": postvars["message"], "timestamp": datetime.datetime.now()}
-
 cursor.execute("INSERT INTO messages VALUES(?);", (pickle.dumps(newMessage),))
+
+time.sleep(0.1) # to get greater timestamp
+newMessage2 = {"user": "pythonbot", "message": "{} is a retard".format(postvars["user"]), "timestamp": datetime.datetime.now()}
+cursor.execute("INSERT INTO messages VALUES(?);", (pickle.dumps(newMessage2),))
+
 connection.commit()
 connection.close()
 
