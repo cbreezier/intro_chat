6eb0778e347c0b9697510b209d5a312938485f65
Davy Mao <e_equals_mass_speed_light_squared@hotmail.com>
2014-02-27 02:41:52 +1100

    more

--- sender.py
+++ sender.py
@@ -13,15 +13,13 @@ postvars = cgi.FieldStorage()
 connection = sqlite3.connect("messages.db")
 cursor = connection.cursor()
 
-posted = {"message_time": "04:20:18 24/03/12"}
-
 returnedMessages = []
 dateTimeFormat = "%I:%M:%S %d/%m/%y"
 for object in cursor.execute("SELECT object FROM messages"):
     print("object is {}".format(object))
     message = pickle.loads(object[0])
     print("message is" + str(message))
-    if message["timestamp"] > datetime.datetime.strptime(posted["message_time"], dateTimeFormat):
+    if not postvars["message_time"] or message["timestamp"] > datetime.datetime.strptime(postvars["message_time"], dateTimeFormat):
         message["message_time"] = message["timestamp"].strftime(dateTimeFormat)
         message.pop("timestamp")
         returnedMessages.append(message)
