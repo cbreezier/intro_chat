51ee11643e2605958372dd06202a81fbe3030d9e
Davy Mao <e_equals_mass_speed_light_squared@hotmail.com>
2014-02-27 01:46:04 +1100

    fixed query

--- receiver.py
+++ receiver.py
@@ -9,12 +9,15 @@ import pickle
 import datetime
 
 postvars = cgi.FieldStorage()
+postvars = {}
 connection = sqlite3.connect("messages.db")
 cursor = connection.cursor()
 
+postvars["user"] = "test"
+postvars["message"] = "test"
 newMessage = {"user": postvars["user"], "message": postvars["message"], "timestamp": datetime.datetime.now()}
 
-cursor.execute("INSERT INTO messages VALUES('?');", [pickle.dumps(newMessage)])
+cursor.execute("INSERT INTO messages VALUES(?);", (pickle.dumps(newMessage),))
 connection.commit()
 connection.close()
 
