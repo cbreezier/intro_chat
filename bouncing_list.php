c170e1474910c20d8fab3f74394b1bba92e6f43c
Leo Huang <crazysheep2302@gmail.com>
2014-05-29 02:43:48 +1000

    Add alert when overwriting ball.py

--- bouncing_list.php
+++ bouncing_list.php
@@ -1,16 +1,21 @@
 <?php
-  echo '<h3>Current bouncing balls</h3>';
   $balls = scandir("bouncingball");
   $bouncecount = file_get_contents("bouncingball/count.txt");
   foreach (explode(PHP_EOL, $bouncecount) as $line) {
     $info = explode(' ', $line);
-    $count[$info[0]] = $info[1];
+    $count[$info[0]] = intval($info[1]);
   }
   foreach ($balls as $ball) {
     if ($ball == '.' || $ball == '..' || !preg_match('/^.*\.py$/', $ball)) {
       continue;
     }
-    $numBounce = isset($count[$ball]) ? $count[$ball] : 0;
-    echo '<span class="ball">'.$ball.' has bounced '.$numBounce.' times</span><br>';
+    if (isset($count[$ball])) {
+      $numBounce = $count[$ball];
+    } else {
+      $numBounce = 0;
+      $count[$ball] = 0;
+    }
+    // echo '<span class="ball">'.$ball.' has bounced '.$numBounce.' times</span><br>';
   }
+  echo json_encode($count);
 ?>
